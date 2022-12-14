a773030df41e0f4a7c685722f3fb6580d0c2d0ac
TamasH <unconfigured@null.spigotmc.org>
2022-12-14 14:06:32 +0100

    wieiswinnaar in while loop gedaan.

--- wieiswinnaar.php
+++ wieiswinnaar.php
@@ -1,14 +1,20 @@
-<?php 
+<?php
 
-echo "voer getal voor speler1 in: ";
-$speler1 = readline();
-echo "voer getal voor speler2 in: ";
-$speler2 = readline();
-
-if ($speler1 > $speler2) {
-    echo "speler1 heeft gewonnen!";
-}else {
-    echo "speler2 heeft gewonnen!";
+echo "type enter om te beginnen of stop om te stoppen.";
+$stop = readline();
+while ($stop != "stop") {
+    echo "voer getal voor speler1 in: ";
+    $speler1 = readline();
+    echo "voer getal voor speler2 in: ";
+    $speler2 = readline();
+    if ($speler1 > $speler2) {
+        echo "speler1 heeft gewonnen!\n";
+    }else {
+        echo "speler2 heeft gewonnen!\n";
+    }
+    echo "Wil je stoppen? [stop]";
+    $stop = readline();
+    strtolower($stop);
 }
 
 ?>
